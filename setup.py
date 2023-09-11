2205c3e2ac4fbe0786d491ee4de2397f53269543
Aleš Hakl <ales.hakl@anthill.cz>
2023-09-11 16:57:19 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name="Flask-Bootstrap-Components",
-    version="0.1.1",
+    version="0.2.1",
     author = "Ales Hakl",
     author_email = "ales@hakl.net",
     description = ("Collection of HTML generation helpers for Flask with Bootstrap 4"),
